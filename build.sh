aa1e8841a2a4284c76d47497271ecfa792c34373
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-11-17 18:09:05 +0800

    add x64 build (untested)

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
-LLVM_BIN="$ANDROID_HOME/ndk/28.1.13356709/toolchains/llvm/prebuilt/linux-x86_64/bin"
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="$LLVM_BIN/aarch64-linux-android21-clang"
+if [ "$1" == "x64" ]; then
+    LLVM_BIN="$ANDROID_HOME/ndk/28.1.13356709/toolchains/llvm/prebuilt/linux-x86_64/bin"
+    export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="$LLVM_BIN/x86_64-linux-android26-clang"
+    TARGET=x86_64-unknown-linux-musl
+else
+    LLVM_BIN="$ANDROID_HOME/ndk/28.1.13356709/toolchains/llvm/prebuilt/linux-x86_64/bin"
+    export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="$LLVM_BIN/aarch64-linux-android21-clang"
+    export RUSTFLAGS="-C link-arg=-no-pie"
+    TARGET=aarch64-unknown-linux-musl
+fi
 
-RUSTFLAGS="-C link-arg=-no-pie" cargo build --target=aarch64-unknown-linux-musl --release
+cargo build --target=$TARGET --release
